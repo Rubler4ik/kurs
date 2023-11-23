c0e5b7358bb47c6b84593709f80a5533749533a1
Dima Grishko <dimagrishkoby@gmail.com>
2023-11-23 11:16:37 +0300

    готово осталось сохр и откр

--- MainWindow.py
+++ MainWindow.py
@@ -3,16 +3,24 @@ from tkinter import *
 
 
 class MainsWindows:
-    def __init__(self, title, text, resizable, icon="materials/sort-2_icon-icons.com_69583.ico"):
+    def __init__(self, title, text, text2, text3,resizable, icon="materials/sort-2_icon-icons.com_69583.ico"):
         self.window = Tk()
         self.window.title(title)
         self.window.iconbitmap(default=icon)
         self.window.resizable(resizable, resizable)
         self.window.option_add("*tearOff", FALSE)
         if text != "":
-            label = ttk.Label(self.window, text=text, justify="center", background="#FFCDD2", font="Arial,30",
+            self.window["bg"] = "#FFCDD2"
+            label = ttk.Label(self.window, text=text, justify="center", background="#FFCDD2",font="Helvetica,40",
                               padding=8)
-            label.pack(expand=True)
+            label.pack(anchor="n")
+            label2= ttk.Label(self.window, text=text2,justify="center", background="#FFCDD2", font="Helvetica,30",
+                              padding=8)
+            label2.pack(expand=True)
+            label3 = ttk.Label(self.window, text=text3, justify="center", background="#FFCDD2", font="Arial,30",
+                               padding=8)
+            label3.pack(expand=True)
+
         self.window.update()
         self.window.geometry(
             f"+{self.window.winfo_screenwidth() // 2 - self.window.winfo_width() // 2}+"
